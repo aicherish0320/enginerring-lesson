2b8b7689490e2c56d5643b46e0f312bf4e9d65f8
aicherish0320 <aicherish0320@163.com>
2022-06-05 21:44:20 +0800

    ProvidePlugin

--- zbestpc/webpack.config.js
+++ zbestpc/webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ProvidePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -34,6 +35,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
+    }),
+    new ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
     })
   ]
 }
