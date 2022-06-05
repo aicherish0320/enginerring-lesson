68bc50d284b2b6b36615e461fbf18340bf843a7f
aicherish0320 <aicherish0320@163.com>
2022-06-05 14:12:51 +0800

    webpack devtool

--- webpack-demo/dist/bundle.js
+++ webpack-demo/dist/bundle.js
@@ -1,11 +1,2 @@
-;(() => {
-  var __webpack_modules__ = {
-    './src/index.js': () => {
-      eval(
-        "console.log('Hello')\n\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?"
-      )
-    }
-  }
-  var __webpack_exports__ = {}
-  __webpack_modules__['./src/index.js']()
-})()
+console.log("Hello");
+//# sourceMappingURL=bundle.js.map
