2af445a797b5149ba8b6f77791b8facd9b12c4db
aicherish0320 <aicherish0320@163.com>
2022-06-16 17:59:44 +0800

    vue2 升级 vue3 原理讲解

---
+++ zbestpc/build/webpack.vue3.config.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ProvidePlugin } = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, '../src/main.js')
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: '3001',
+    hot: true
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 300 * 1024,
+      name: 'common',
+      cacheGroups: {
+        jquery: {
+          name: 'jquery',
+          test: /jquery/,
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:6][ext]'
+        }
+      },
+      {
+        test: /\.ejs$/,
+        loader: 'ejs-loader',
+        options: {
+          esModule: false
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      chunks: ['index']
+    }),
+    new ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/img'),
+          to: path.resolve(__dirname, '../dist/img')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].chunk.css'
+    }),
+    new VueLoaderPlugin
+  ]
+}
+
+
+module.exports = config
